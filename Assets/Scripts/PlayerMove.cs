c072cd13f7e5b321852f78c2adc7c785e4c163d4
ggugguggagga <gustj5084@naver.com>
2022-04-05 15:16:18 +0900

    change player move method & delete joystick

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -2,33 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMove : MonoBehaviour
-{
+public class PlayerMove : MonoBehaviour {
     // static으로 선언된 PlayerMove
     // Player는 단 하나이므로 플레이어에 대한 참조가 필요할 경우 PlayerMove.Player 로 접근
     public static PlayerMove Player;
 
-    // 조이스틱 참조
-    public Joystick mJoystick;
-
     // Player 이동속도
     public float playerMoveSpeed;
+
+    //시간 * 스피드 움직임 값
+    float moveX, moveY;
     
-    void Awake()
-    {
+    void Awake() {
         // 초기화 작업
         if(Player == null)
             Player = this;
-        if(mJoystick == null)
-            mJoystick = GameObject.Find("Floating Joystick").GetComponent<Joystick>();
     }
 
-    void Update()
-    {
-        // 플레이어 이동
-        if(mJoystick != null){
-            transform.Translate(new Vector3(mJoystick.Horizontal,mJoystick.Vertical,0) * playerMoveSpeed * Time.deltaTime);
-            Debug.Log(mJoystick.Horizontal + ", " + mJoystick.Vertical);
-        }
+    void Update(){
+        moveX = Input.GetAxis("Horizontal") * playerMoveSpeed * Time.deltaTime;
+        moveY = Input.GetAxis("Vertical") * playerMoveSpeed * Time.deltaTime;
+
+        transform.position = new Vector2(moveX + transform.position.x, moveY + transform.position.y);
+
     }
 }

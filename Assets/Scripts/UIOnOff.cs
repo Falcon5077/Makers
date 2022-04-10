320ae2f184a49f04db90becd69b8d273be0d73ad
Parcon <jklcvb1@gmail.com>
2022-04-10 11:59:39 +0900

    Sound Controller

--- Assets/Scripts/UIOnOff.cs
+++ Assets/Scripts/UIOnOff.cs
@@ -4,18 +4,6 @@ using UnityEngine;
 
 public class UIOnOff : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void UI_SetActive(GameObject UI_Object)
     {
         UI_Object.SetActive(true);

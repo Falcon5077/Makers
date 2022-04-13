e88c23cfbcc239eb0bf2989241574daee82e7e30
JeonJuSeong <mo66jyp@gmail.com>
2022-04-13 21:15:38 +0900

    원거리적 총알 발사 추가
    원거리적이 이제 총알을 발사합니다.
    기존 player가 발사하던 총알을 "Player Bullet" 과 "Enemy Bullet"으로 나눠 색과 속도를 다르게 만들었습니다.
    원거리 적은 빨간색 총알을, 플레이어는 초록색 총알을 발사합니다.
    원거리적의 Fire Time 변수를 이용해 발사 속도를 변경할 수 있습니다.
    총알속도는 각 bullet 에서 변경할 수 있습니다.

--- Assets/Scripts/Distant_Enemy.cs
+++ Assets/Scripts/Distant_Enemy.cs
@@ -5,8 +5,17 @@ using UnityEngine;
 public class Distant_Enemy : MonoBehaviour
 {
     private Transform target;
+    private bool attacking;
+    private Vector2 attackAngle;
     [SerializeField]private float distance;
     [SerializeField]private float moveSpeed;
+    [SerializeField]private bool shoot = true;
+    public Transform bulletPoint;
+    public GameObject bulletPrefab;
+    float shootTime;
+    // 발사 간격
+    [SerializeField] float fireTime;
+    // 발사해야할 시간
     // Start is called before the first frame update
     void Start()
     {
@@ -17,11 +26,27 @@ public class Distant_Enemy : MonoBehaviour
     void Update()
     {
         FollowTarget();
+        bulletFire();
+        //Invoke("FollowTarget",0.5f); //? 이거 제대로 실행되지 않는거 같아요
     }
 
     private void FollowTarget() {
         if(Vector2.Distance(transform.position, target.position) > distance) {
             transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed*Time.deltaTime);
         }else transform.position = Vector2.MoveTowards(transform.position, target.position, -moveSpeed*Time.deltaTime);
+    }
+    
+    //총알 발사 코드
+    void bulletFire(){
+        if (shoot){
+            Vector2 direction = target.position - transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+            if (shootTime <= Time.time){
+                Instantiate(bulletPrefab, transform.position, Quaternion.AngleAxis(angle - 90, Vector3.forward));
+                shootTime = Time.time + fireTime;
+            }
+        }
+        
     }
 }

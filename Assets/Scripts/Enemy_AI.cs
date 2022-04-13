adb6e99b397ed02720a3d091947d00d3efdee7ee
JeonJuSeong <mo66jyp@gmail.com>
2022-04-13 19:01:12 +0900

    원거리적 추가 / 근거리적 수정
    원거리적 entity 추가, 근거리 적 필요없는 if문(collider 내 진입시 이동체크) 삭제, Unity 내에서 바로 Enemy speed 변경할 수 있게 Serialize

--- Assets/Scripts/Enemy_AI.cs
+++ Assets/Scripts/Enemy_AI.cs
@@ -6,12 +6,10 @@ public class Enemy_AI : MonoBehaviour
 {
     Rigidbody2D rb;
     Transform target;
-    [Header("추격 속도")]
-    [SerializeField][Range(1f, 4f)] float moveSpeed = 3f;
+    [SerializeField]float moveSpeed;
 
-    [Header("근접 거리")]
-    [SerializeField][Range(0f, 3f)] float contactDistance = 1f;
-    //bool follow = false;
+    //float contactDistance = 1f; // Player 감지거리
+    //bool follow = false; // Player가 Collider내에 들어왔는지 체크를 위한 bool변수 (false: 아님, true: 들어옴)
     // Start is called before the first frame update
     void Start()
     {
@@ -25,11 +23,17 @@ public class Enemy_AI : MonoBehaviour
     }
 
     void FollowTarget() {
+        /*
+        Enemy의 Circle Collider안에 들어왔을 시 이동하는 조건으로 작동시키고 싶을 때 아래의 코드 대신 해당 코드 사용
         if(Vector2.Distance(transform.position, target.position) > contactDistance)
             transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
         else rb.velocity = Vector2.zero;
+        */
+
+        transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
     }
     /*
+    Enemy의 Circle Collider 안에 들어왔을 시 이동하는 조건으로 작동시키고 싶을 시 해당 코드내용 사용
     private void OnTriggerEnter2D(Collider2D collision) {   //Enemy의 Circle Collison내에 Player가 들어오게 되면 follow = true로 세팅
         follow = true;
     }

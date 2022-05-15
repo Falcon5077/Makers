eecbd70a3add5e89aac7814597d7597bdb1e5bfe
Falcon <royale@kakao.com>
2022-05-15 16:57:53 +0900

    EnemyLimit

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     public GameObject Enemy;
+    public List<GameObject> EnemyList = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
@@ -12,12 +13,25 @@ public class EnemySpawner : MonoBehaviour
         
     }
 
-    void SpawnEnemy()
+    async void SpawnEnemy()
     {
-        float x = Random.Range(-10,10);
-        float y = Random.Range(-10,10);
-        Instantiate(Enemy,new Vector3(x,y,0),Quaternion.identity);
+        for(int i = 0; i < EnemyList.Count; i++)
+        {
+            if(EnemyList[i] == null)
+                EnemyList.RemoveAt(i);
 
+            Debug.Log("Remove");
+        }
+        if(EnemyList.Count <= 4)
+        {
+            float x = Random.Range(-10,10);
+            float y = Random.Range(-10,10);
+            
+            EnemyList.Add(Instantiate(Enemy,new Vector3(x,y,0),Quaternion.identity));
+        }
+        else
+            Debug.Log(EnemyList.Count + "It's Full");
+        
         Invoke("SpawnEnemy",Random.Range(1.5f,3f));
     }
     // Update is called once per frame
